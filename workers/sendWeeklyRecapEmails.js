7b46a1f7973df8e2190d2bd1129f9b11372ffaba
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2015-05-15 16:46:55 +0200

    add fourth week

--- workers/sendWeeklyRecapEmails.js
+++ workers/sendWeeklyRecapEmails.js
@@ -359,6 +359,11 @@ var worker = function(job, done){
     passedTargetDateBegin = moment.utc().subtract(8, 'days').startOf('day').toDate();
     passedTargetDateEnd = moment(passedTargetDateBegin).add(1, 'days').toDate();
     findAffectedUsers(passedTargetDateBegin, passedTargetDateEnd, 1);
+  }else if(parseInt(job.data.weeklyPhase) === 4){
+    console.log('Starting phase 4');
+    passedTargetDateBegin = moment.utc().subtract(29, 'days').startOf('day').toDate();
+    passedTargetDateEnd = moment(passedTargetDateBegin).add(1, 'days').toDate();
+    findAffectedUsers(passedTargetDateBegin, passedTargetDateEnd, 4);
   }else{
     console.log('Starting phase 3');
     passedTargetDateBegin = moment.utc().subtract(22, 'days').startOf('day').toDate();
