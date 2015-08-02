15d3412c8d8d70cbdcc46659da9402229bb2ff90
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2015-08-02 13:27:48 +0200

    try watching for stuck jobs

--- index.js
+++ index.js
@@ -63,6 +63,8 @@ queue.on('job failed', function(){
   console.error.apply(console, args);
 });
 
+queue.watchStuckJobs();
+
 process.once('uncaughtException', function(err){
   queue.shutdown(9500, function(err2){
     process.exit(0);
