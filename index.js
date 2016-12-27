1cfcba7b70072752b8fcaca1e421794aee82ea25
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2016-12-27 17:02:10 +0100

    lint index and utils

--- index.js
+++ index.js
@@ -1,78 +1,80 @@
-var kue = require('kue'),
-    express = require('express'),
-    monk = require('monk'),
-    url = require('url'),
-    nconf = require('nconf');
+const kue = require('kue');
+const express = require('express');
+const monk = require('monk');
+const url = require('url');
+const nconf = require('nconf');
+const AWS = require('aws-sdk');
 
 nconf
   .argv()
   .env()
-  .file({ file: __dirname + '/config.json' });
+  .file({ file: `${__dirname  }/config.json` });
 
-var app = express();
-
-var baseUrl = 'https://habitica.com';
-
-var db = monk(nconf.get('MONGODB_URL'));
+const app = express();
+const db = monk(nconf.get('MONGODB_URL'));
 db.options.multi = true;
 
-var AWS = require('aws-sdk');
+const BASE_URL = 'https://habitica.com';
 
 AWS.config.update({
   accessKeyId: nconf.get('AWS_ACCESS_KEY'),
-  secretAccessKey: nconf.get('AWS_SECRET_KEY')
+  secretAccessKey: nconf.get('AWS_SECRET_KEY'),
 });
 
-var kueRedisOpts = {
+let kueRedisOpts = {
   port: nconf.get('REDIS_PORT'),
-  host: nconf.get('REDIS_HOST')
+  host: nconf.get('REDIS_HOST'),
 };
 
-if(nconf.get('NODE_ENV') === 'production'){
-  var redisURL = url.parse(process.env.REDIS_URL);
-  kueRedisOpts.auth = redisURL.auth.split(":")[1];
+if (nconf.get('NODE_ENV') === 'production') {
+  let redisURL = url.parse(nconf.get('REDIS_URL'));
+  kueRedisOpts.auth = redisURL.auth.split(':')[1];
   kueRedisOpts.host = redisURL.hostname;
   kueRedisOpts.port = redisURL.port;
 }
 
-var queue = kue.createQueue({
+let queue = kue.createQueue({
   disableSearch: true,
-  redis: kueRedisOpts
+  redis: kueRedisOpts,
 });
 
 queue.process('email', 30, require('./workers/email'));
-queue.process('sendBatchEmails', require('./workers/sendBatchEmails')(queue, db, baseUrl));
-// queue.process('sendWeeklyRecapEmails', require('./workers/sendWeeklyRecapEmails')(queue, db, baseUrl));
-queue.process('amazonPayments', require('./workers/amazonPayments')(queue, db));
-queue.process('amazonGroupPlanPayments', require('./workers/amazonGroupPlanPayments')(queue, db));
+
+queue.process('sendBatchEmails', require('./workers/sendBatchEmails')(queue, db, BASE_URL));
+queue.process('sendOnboardingEmails', require('./workers/sendOnboardingEmails')(queue, db));
+// queue.process('sendWeeklyRecapEmails', require('./workers/sendWeeklyRecapEmails')(queue, db, BASE_URL));
+
 queue.process('sendSpecialPushNotifications', require('./workers/sendSpecialPushNotifications')(queue, db));
 
-queue.on('job complete', function(id, result){
-  kue.Job.get(id, function(err, job){
-    if(err) return;
-    job.remove(function(err){
-      if(err) throw err;
+queue.process('amazonPayments', require('./workers/amazonPayments')(queue, db));
+queue.process('amazonGroupPlanPayments', require('./workers/amazonGroupPlanPayments')(queue, db));
+
+queue.on('job complete', (id) => {
+  kue.Job.get(id, (err, job) => {
+    if (err) return;
+    job.remove((err2) => {
+      if (err2) throw err2;
     });
   });
 });
 
-queue.on('job failed', function(){
-  var args = Array.prototype.slice.call(arguments);
+queue.on('job failed', () => {
+  let args = Array.prototype.slice.call(arguments);
   args.unshift('Error processing job.');
-  console.error.apply(console, JSON.stringify(args));
+  console.error(...JSON.stringify(args));
 });
 
 queue.watchStuckJobs();
 
-process.once('uncaughtException', function(err){
-  queue.shutdown(9500, function(err2){
+process.once('uncaughtException', (err) => {
+  queue.shutdown(9500, (err2) => {
     console.log('Kue is shutting down.', err, err2);
     process.exit(0);
   });
 });
 
-process.once('SIGTERM', function(sig){
-  queue.shutdown(9500, function(err) {
+process.once('SIGTERM', () => {
+  queue.shutdown(9500, (err) => {
     console.log('Kue is shutting down.', err || '');
     process.exit(0);
   });
@@ -81,4 +83,5 @@ process.once('SIGTERM', function(sig){
 app.use(require('basic-auth-connect')(nconf.get('AUTH_USER'), nconf.get('AUTH_PASSWORD')));
 app.use(kue.app);
 app.listen(nconf.get('PORT'));
-console.log('Server listening on port ' + nconf.get('PORT'));
+
+console.log(`Server listening on port ${  nconf.get('PORT')}`);
