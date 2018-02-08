430f9ae2d0541ef5ebe4e3c9aad9a4937fe32b08
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2018-02-08 11:58:03 +0100

    update crypto algo

--- utils.js
+++ utils.js
@@ -3,10 +3,15 @@ const nconf = require('nconf');
 
 module.exports = {};
 
-const ALGORITHM = 'aes-256-ctr';
+const algorithm = 'aes-256-ctr';
+const SESSION_SECRET_KEY = nconf.get('SESSION_SECRET_KEY');
+const SESSION_SECRET_IV = nconf.get('SESSION_SECRET_IV');
+
+let key = Buffer.from(SESSION_SECRET_KEY, 'hex');
+let iv  = Buffer.from(SESSION_SECRET_IV, 'hex');
 
 module.exports.encrypt = function encrypt (text) {
-  let cipher = crypto.createCipher(ALGORITHM, nconf.get('SESSION_SECRET'));
+  let cipher = crypto.createCipheriv(algorithm, key, iv);
   let crypted = cipher.update(text, 'utf8', 'hex');
   crypted += cipher.final('hex');
   return crypted;
